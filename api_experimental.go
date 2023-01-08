046457cf6b9fb6f8f10d75907f43db13bf014945
williamfzc <178894043@qq.com>
2023-01-08 16:47:47 +0800

    sync with v0.11.1

--- api_experimental.go
+++ api_experimental.go
@@ -22,6 +22,136 @@ import (
 // EXPERIMENTALApiService EXPERIMENTALApi service
 type EXPERIMENTALApiService service
 
+type ApiApiV1ClazzWithRuleGetRequest struct {
+	ctx context.Context
+	ApiService *EXPERIMENTALApiService
+	repo *string
+	rev *string
+	rule *string
+}
+
+// repo
+func (r ApiApiV1ClazzWithRuleGetRequest) Repo(repo string) ApiApiV1ClazzWithRuleGetRequest {
+	r.repo = &repo
+	return r
+}
+
+// rev
+func (r ApiApiV1ClazzWithRuleGetRequest) Rev(rev string) ApiApiV1ClazzWithRuleGetRequest {
+	r.rev = &rev
+	return r
+}
+
+// rule
+func (r ApiApiV1ClazzWithRuleGetRequest) Rule(rule string) ApiApiV1ClazzWithRuleGetRequest {
+	r.rule = &rule
+	return r
+}
+
+func (r ApiApiV1ClazzWithRuleGetRequest) Execute() ([]Sibyl2ClazzWithPath, *http.Response, error) {
+	return r.ApiService.ApiV1ClazzWithRuleGetExecute(r)
+}
+
+/*
+ApiV1ClazzWithRuleGet clazz query
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @return ApiApiV1ClazzWithRuleGetRequest
+*/
+func (a *EXPERIMENTALApiService) ApiV1ClazzWithRuleGet(ctx context.Context) ApiApiV1ClazzWithRuleGetRequest {
+	return ApiApiV1ClazzWithRuleGetRequest{
+		ApiService: a,
+		ctx: ctx,
+	}
+}
+
+// Execute executes the request
+//  @return []Sibyl2ClazzWithPath
+func (a *EXPERIMENTALApiService) ApiV1ClazzWithRuleGetExecute(r ApiApiV1ClazzWithRuleGetRequest) ([]Sibyl2ClazzWithPath, *http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodGet
+		localVarPostBody     interface{}
+		formFiles            []formFile
+		localVarReturnValue  []Sibyl2ClazzWithPath
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "EXPERIMENTALApiService.ApiV1ClazzWithRuleGet")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/api/v1/clazz/with/rule"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+	if r.repo == nil {
+		return localVarReturnValue, nil, reportError("repo is required and must be specified")
+	}
+	if r.rev == nil {
+		return localVarReturnValue, nil, reportError("rev is required and must be specified")
+	}
+	if r.rule == nil {
+		return localVarReturnValue, nil, reportError("rule is required and must be specified")
+	}
+
+	localVarQueryParams.Add("repo", parameterToString(*r.repo, ""))
+	localVarQueryParams.Add("rev", parameterToString(*r.rev, ""))
+	localVarQueryParams.Add("rule", parameterToString(*r.rule, ""))
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := ioutil.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = ioutil.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
 type ApiApiV1FuncSignatureGetRequest struct {
 	ctx context.Context
 	ApiService *EXPERIMENTALApiService
@@ -358,6 +488,136 @@ func (a *EXPERIMENTALApiService) ApiV1FuncWithSignatureGetExecute(r ApiApiV1Func
 	localVarQueryParams.Add("repo", parameterToString(*r.repo, ""))
 	localVarQueryParams.Add("rev", parameterToString(*r.rev, ""))
 	localVarQueryParams.Add("signature", parameterToString(*r.signature, ""))
+	// to determine the Content-Type header
+	localVarHTTPContentTypes := []string{}
+
+	// set Content-Type header
+	localVarHTTPContentType := selectHeaderContentType(localVarHTTPContentTypes)
+	if localVarHTTPContentType != "" {
+		localVarHeaderParams["Content-Type"] = localVarHTTPContentType
+	}
+
+	// to determine the Accept header
+	localVarHTTPHeaderAccepts := []string{"application/json"}
+
+	// set Accept header
+	localVarHTTPHeaderAccept := selectHeaderAccept(localVarHTTPHeaderAccepts)
+	if localVarHTTPHeaderAccept != "" {
+		localVarHeaderParams["Accept"] = localVarHTTPHeaderAccept
+	}
+	req, err := a.client.prepareRequest(r.ctx, localVarPath, localVarHTTPMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, formFiles)
+	if err != nil {
+		return localVarReturnValue, nil, err
+	}
+
+	localVarHTTPResponse, err := a.client.callAPI(req)
+	if err != nil || localVarHTTPResponse == nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	localVarBody, err := ioutil.ReadAll(localVarHTTPResponse.Body)
+	localVarHTTPResponse.Body.Close()
+	localVarHTTPResponse.Body = ioutil.NopCloser(bytes.NewBuffer(localVarBody))
+	if err != nil {
+		return localVarReturnValue, localVarHTTPResponse, err
+	}
+
+	if localVarHTTPResponse.StatusCode >= 300 {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: localVarHTTPResponse.Status,
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	err = a.client.decode(&localVarReturnValue, localVarBody, localVarHTTPResponse.Header.Get("Content-Type"))
+	if err != nil {
+		newErr := &GenericOpenAPIError{
+			body:  localVarBody,
+			error: err.Error(),
+		}
+		return localVarReturnValue, localVarHTTPResponse, newErr
+	}
+
+	return localVarReturnValue, localVarHTTPResponse, nil
+}
+
+type ApiApiV1FuncctxWithRuleGetRequest struct {
+	ctx context.Context
+	ApiService *EXPERIMENTALApiService
+	repo *string
+	rev *string
+	rule *string
+}
+
+// repo
+func (r ApiApiV1FuncctxWithRuleGetRequest) Repo(repo string) ApiApiV1FuncctxWithRuleGetRequest {
+	r.repo = &repo
+	return r
+}
+
+// rev
+func (r ApiApiV1FuncctxWithRuleGetRequest) Rev(rev string) ApiApiV1FuncctxWithRuleGetRequest {
+	r.rev = &rev
+	return r
+}
+
+// rule
+func (r ApiApiV1FuncctxWithRuleGetRequest) Rule(rule string) ApiApiV1FuncctxWithRuleGetRequest {
+	r.rule = &rule
+	return r
+}
+
+func (r ApiApiV1FuncctxWithRuleGetRequest) Execute() ([]Sibyl2FunctionContext, *http.Response, error) {
+	return r.ApiService.ApiV1FuncctxWithRuleGetExecute(r)
+}
+
+/*
+ApiV1FuncctxWithRuleGet func ctx query
+
+ @param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+ @return ApiApiV1FuncctxWithRuleGetRequest
+*/
+func (a *EXPERIMENTALApiService) ApiV1FuncctxWithRuleGet(ctx context.Context) ApiApiV1FuncctxWithRuleGetRequest {
+	return ApiApiV1FuncctxWithRuleGetRequest{
+		ApiService: a,
+		ctx: ctx,
+	}
+}
+
+// Execute executes the request
+//  @return []Sibyl2FunctionContext
+func (a *EXPERIMENTALApiService) ApiV1FuncctxWithRuleGetExecute(r ApiApiV1FuncctxWithRuleGetRequest) ([]Sibyl2FunctionContext, *http.Response, error) {
+	var (
+		localVarHTTPMethod   = http.MethodGet
+		localVarPostBody     interface{}
+		formFiles            []formFile
+		localVarReturnValue  []Sibyl2FunctionContext
+	)
+
+	localBasePath, err := a.client.cfg.ServerURLWithContext(r.ctx, "EXPERIMENTALApiService.ApiV1FuncctxWithRuleGet")
+	if err != nil {
+		return localVarReturnValue, nil, &GenericOpenAPIError{error: err.Error()}
+	}
+
+	localVarPath := localBasePath + "/api/v1/funcctx/with/rule"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+	if r.repo == nil {
+		return localVarReturnValue, nil, reportError("repo is required and must be specified")
+	}
+	if r.rev == nil {
+		return localVarReturnValue, nil, reportError("rev is required and must be specified")
+	}
+	if r.rule == nil {
+		return localVarReturnValue, nil, reportError("rule is required and must be specified")
+	}
+
+	localVarQueryParams.Add("repo", parameterToString(*r.repo, ""))
+	localVarQueryParams.Add("rev", parameterToString(*r.rev, ""))
+	localVarQueryParams.Add("rule", parameterToString(*r.rule, ""))
 	// to determine the Content-Type header
 	localVarHTTPContentTypes := []string{}
 
--- docs/EXPERIMENTALApi.md
+++ docs/EXPERIMENTALApi.md
@@ -4,10 +4,80 @@ All URIs are relative to *http://localhost*
 
 Method | HTTP request | Description
 ------------- | ------------- | -------------
+[**ApiV1ClazzWithRuleGet**](EXPERIMENTALApi.md#ApiV1ClazzWithRuleGet) | **Get** /api/v1/clazz/with/rule | clazz query
 [**ApiV1FuncSignatureGet**](EXPERIMENTALApi.md#ApiV1FuncSignatureGet) | **Get** /api/v1/func/signature | func query
 [**ApiV1FuncWithRuleGet**](EXPERIMENTALApi.md#ApiV1FuncWithRuleGet) | **Get** /api/v1/func/with/rule | func query
 [**ApiV1FuncWithSignatureGet**](EXPERIMENTALApi.md#ApiV1FuncWithSignatureGet) | **Get** /api/v1/func/with/signature | func query
+[**ApiV1FuncctxWithRuleGet**](EXPERIMENTALApi.md#ApiV1FuncctxWithRuleGet) | **Get** /api/v1/funcctx/with/rule | func ctx query
 
+
+
+## ApiV1ClazzWithRuleGet
+
+> []Sibyl2ClazzWithPath ApiV1ClazzWithRuleGet(ctx).Repo(repo).Rev(rev).Rule(rule).Execute()
+
+clazz query
+
+### Example
+
+```go
+package main
+
+import (
+    "context"
+    "fmt"
+    "os"
+    openapiclient "./openapi"
+)
+
+func main() {
+    repo := "repo_example" // string | repo
+    rev := "rev_example" // string | rev
+    rule := "rule_example" // string | rule
+
+    configuration := openapiclient.NewConfiguration()
+    apiClient := openapiclient.NewAPIClient(configuration)
+    resp, r, err := apiClient.EXPERIMENTALApi.ApiV1ClazzWithRuleGet(context.Background()).Repo(repo).Rev(rev).Rule(rule).Execute()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error when calling `EXPERIMENTALApi.ApiV1ClazzWithRuleGet``: %v\n", err)
+        fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+    }
+    // response from `ApiV1ClazzWithRuleGet`: []Sibyl2ClazzWithPath
+    fmt.Fprintf(os.Stdout, "Response from `EXPERIMENTALApi.ApiV1ClazzWithRuleGet`: %v\n", resp)
+}
+```
+
+### Path Parameters
+
+
+
+### Other Parameters
+
+Other parameters are passed through a pointer to a apiApiV1ClazzWithRuleGetRequest struct via the builder pattern
+
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **repo** | **string** | repo | 
+ **rev** | **string** | rev | 
+ **rule** | **string** | rule | 
+
+### Return type
+
+[**[]Sibyl2ClazzWithPath**](Sibyl2ClazzWithPath.md)
+
+### Authorization
+
+No authorization required
+
+### HTTP request headers
+
+- **Content-Type**: Not defined
+- **Accept**: application/json
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints)
+[[Back to Model list]](../README.md#documentation-for-models)
+[[Back to README]](../README.md)
 
 
 ## ApiV1FuncSignatureGet
@@ -199,6 +269,74 @@ Name | Type | Description  | Notes
 ### Return type
 
 [**Sibyl2FunctionWithPath**](Sibyl2FunctionWithPath.md)
+
+### Authorization
+
+No authorization required
+
+### HTTP request headers
+
+- **Content-Type**: Not defined
+- **Accept**: application/json
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints)
+[[Back to Model list]](../README.md#documentation-for-models)
+[[Back to README]](../README.md)
+
+
+## ApiV1FuncctxWithRuleGet
+
+> []Sibyl2FunctionContext ApiV1FuncctxWithRuleGet(ctx).Repo(repo).Rev(rev).Rule(rule).Execute()
+
+func ctx query
+
+### Example
+
+```go
+package main
+
+import (
+    "context"
+    "fmt"
+    "os"
+    openapiclient "./openapi"
+)
+
+func main() {
+    repo := "repo_example" // string | repo
+    rev := "rev_example" // string | rev
+    rule := "rule_example" // string | rule
+
+    configuration := openapiclient.NewConfiguration()
+    apiClient := openapiclient.NewAPIClient(configuration)
+    resp, r, err := apiClient.EXPERIMENTALApi.ApiV1FuncctxWithRuleGet(context.Background()).Repo(repo).Rev(rev).Rule(rule).Execute()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "Error when calling `EXPERIMENTALApi.ApiV1FuncctxWithRuleGet``: %v\n", err)
+        fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", r)
+    }
+    // response from `ApiV1FuncctxWithRuleGet`: []Sibyl2FunctionContext
+    fmt.Fprintf(os.Stdout, "Response from `EXPERIMENTALApi.ApiV1FuncctxWithRuleGet`: %v\n", resp)
+}
+```
+
+### Path Parameters
+
+
+
+### Other Parameters
+
+Other parameters are passed through a pointer to a apiApiV1FuncctxWithRuleGetRequest struct via the builder pattern
+
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **repo** | **string** | repo | 
+ **rev** | **string** | rev | 
+ **rule** | **string** | rule | 
+
+### Return type
+
+[**[]Sibyl2FunctionContext**](Sibyl2FunctionContext.md)
 
 ### Authorization
 
